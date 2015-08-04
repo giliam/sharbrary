af8fe0b1f45f3c2bf96942a2d7b29491a9a20490
giliam <matthieu.denoux@gmail.com>
2015-08-04 14:00:39 +0200

    Renames it to sharbrary.

--- sharbrary/wsgi.py
+++ sharbrary/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 sys.path.append('/usr/local/django')
-sys.path.append('/home/django/projet/bibliournante')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bibliournante.settings")
+sys.path.append('/home/django/projet/sharbrary')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sharbrary.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

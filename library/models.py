bd49a3b61b0d50084f6f33bdfe31369249e55082
giliam <matthieu.denoux@gmail.com>
2015-08-04 13:00:46 +0200

    Adds a few models to the library.

--- library/models.py
+++ library/models.py
@@ -11,11 +11,27 @@ class Author(models.Model):
     def __unicode__(self):
         return self.firstname + " " + self.lastname.upper()
 
+class Editor(models.Model):
+    name = models.CharField(max_length=200)    
+
+    def __unicode__(self):
+        return "Editor: " + self.name
+
+class Theme(models.Model):
+    name = models.CharField(max_length=200)
+    period = models.CharField(max_length=200)
+    adding_date = models.DateTimeField('date added to the database',auto_now_add=True)
+
+    def __unicode__(self):
+        return "Theme: " + self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publishing_date = models.DateTimeField('date published')
     adding_date = models.DateTimeField('date added to the library',auto_now_add=True)
     author = models.ForeignKey(Author)
+    themes = models.ForeignKey(Theme)
+    summary = models.TextField()
 
     def __unicode__(self):
         return self.title

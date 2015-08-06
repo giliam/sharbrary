32f4595b152b4cb4cabbe694da366085093c8960
giliam <matthieu.denoux@gmail.com>
2015-08-06 11:00:56 -0400

    Fixes a few issues on static files.

--- sharbrary/settings.py
+++ sharbrary/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = FORCE_SCRIPT_NAME + "/static/"
 STATIC_ROOT = BASE_DIR + '/static/'
 
 STATICFILES_DIRS = parameters.STATICFILES_DIRS if parameters.STATICFILES_DIRS else (

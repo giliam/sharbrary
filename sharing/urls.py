cd00214b17f9e4869551fcac2c5b3edb37cdb4ba
giliam <matthieu.denoux@gmail.com>
2015-08-10 10:44:23 +0200

    Adds the permissions on the sharing views.

--- sharing/urls.py
+++ sharing/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import permission_required
 
 from sharing import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.LendingList.as_view(), name='lending_list'),
-  url(r'^lending/new$', views.LendingCreate.as_view(), name='lending_new'),
-  url(r'^lending/edit/(?P<pk>\d+)$', views.LendingUpdate.as_view(), name='lending_edit'),
-  url(r'^lending/delete/(?P<pk>\d+)$', views.LendingDelete.as_view(), name='lending_delete'),
-  url(r'^lending/end/(?P<lending_id>\d+)$', 'sharing.views.end_lending', name='lending_end'),
-  url(r'^borrowers/$', views.BorrowerList.as_view(), name='borrower_list'),
+  url(r'^$', permission_required('sharing.lending_list')(views.LendingList.as_view()), name='lending_list'),
+  url(r'^lending/new$', permission_required('sharing.lending_new')(views.LendingCreate.as_view()), name='lending_new'),
+  url(r'^lending/edit/(?P<pk>\d+)$', permission_required('sharing.lending_edit')(views.LendingUpdate.as_view()), name='lending_edit'),
+  url(r'^lending/delete/(?P<pk>\d+)$', permission_required('sharing.lending_delete')(views.LendingDelete.as_view()), name='lending_delete'),
+  url(r'^lending/end/(?P<lending_id>\d+)$', permission_required('sharing.end_lending')(views.end_lending), name='lending_end'),
+  url(r'^borrowers/$', permission_required('sharing.borrower_list')(views.BorrowerList.as_view()), name='borrower_list'),
   url(r'^login/$', views.log_in, name='login'),
   url(r'^logout/$', views.log_out, name='logout'),
 )

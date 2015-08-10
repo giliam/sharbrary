ae24bb23b2d34fd8bf7de518f560c1aa5981e400
giliam <matthieu.denoux@gmail.com>
2015-08-10 09:22:34 +0200

    Adds a profile to users.

--- sharing/models.py
+++ sharing/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from library.models import Book
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import RegexValidator
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    informations = models.TextField(blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message=_("Phone number must be entered in the format: '0123456789'. Up to 15 digits allowed."))
+    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=25)
+
+    def __unicode__(self):
+        return u"Profile of {0}".format(self.user.username)
 
 class Lending(models.Model):
     borrower = models.ForeignKey(User,blank=True,verbose_name=_("borrower"))

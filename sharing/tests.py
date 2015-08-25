0926e25367ac5bd8e1b1f0cc9d4d52426a826bc4
giliam <matthieu.denoux@gmail.com>
2015-08-25 16:08:45 +0200

    Prepares sharing tests.

--- sharing/tests.py
+++ sharing/tests.py
@@ -1,4 +1,10 @@
 # coding: utf-8
 from django.test import TestCase
+from django.contrib.auth.models import User, Permission
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import Group
 
-# Create your tests here.
+from utils.tests.common_test_case import CommonTestCase
+
+from sharing.models import Profile, Lending, Queue
